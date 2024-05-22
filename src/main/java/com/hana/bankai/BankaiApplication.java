ca63e6b3e695dd8f81366c557e48199448ec319c
soyeonvv <sovvworld@gmail.com>
2024-05-22 09:16:25 +0900

    [chore] 프로젝트 생성

---
+++ src/main/java/com/hana/bankai/BankaiApplication.java
@@ -0,0 +1,13 @@
+package com.hana.bankai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hana/bankai/BankaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana.bankai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
