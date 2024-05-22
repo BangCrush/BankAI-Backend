b33a4f724f15266ba4981483e0e50ab46142000b
soyeonvv <sovvworld@gmail.com>
2024-05-22 14:42:06 +0900

    [feat] 계좌번호 생성 테스트
    Co-authored-by: Wonhee0221 <totohwh3601@naver.com>
    Co-authored-by: LimEunSang <dmstkd2905@naver.com>

--- src/test/java/com/hana/bankai/BankaiApplicationTests.java
+++ src/test/java/com/hana/bankai/BankaiApplicationTests.java
@@ -1,13 +1,16 @@
 package com.hana.bankai;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class BankaiApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("RUN TEST PROGRAM");
     }
 
 }
---
+++ src/test/java/com/hana/bankai/account/AccountInsertTest.java
@@ -0,0 +1,34 @@
+package com.hana.bankai.account;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.security.SecureRandom;
+
+@Slf4j
+@SpringBootTest
+class AccountInsertTest {
+
+    private static final SecureRandom random = new SecureRandom();
+
+    private String generateTestAccCode() {
+        StringBuilder accCode = new StringBuilder(16);
+        accCode.append("04-");
+        for (int i = 0; i < 5; i++) {
+            accCode.append(random.nextInt(10));
+        }
+        accCode.append("-");
+        for (int i = 0; i < 7; i++) {
+            accCode.append(random.nextInt(10));
+        }
+        return accCode.toString();
+    }
+
+    @Test
+    void 계좌생성테스트() {
+        // 테스트 계정 코드 생성
+        String accCode = generateTestAccCode();
+        // 로그에 출력하여 생성된 코드를 확인
+        log.info("Generated Account Code: {}", accCode);
+    }
+}
