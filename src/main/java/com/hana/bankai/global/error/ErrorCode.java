e371379e56cbe3e817e617fd4fb336f7c63ff062
soyeonvv <sovvworld@gmail.com>
2024-05-23 11:00:44 +0900

    [feat] 예외처리: ErrorCode 설정

---
+++ src/main/java/com/hana/bankai/global/error/ErrorCode.java
@@ -0,0 +1,13 @@
+package com.hana.bankai.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    // User
+    USER_NOT_FOUND("존재하지 않는 회원입니다.");
+
+    private final String message;
+}
