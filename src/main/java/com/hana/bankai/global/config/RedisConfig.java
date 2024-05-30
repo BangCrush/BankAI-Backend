a14e9e0637453e30508ce0f8cfd5f65d385c8c0b
soyeonvv <sovvworld@gmail.com>
2024-05-30 09:13:03 +0900

    [feat] RedissonConfig 설정

--- src/main/java/com/hana/bankai/global/config/RedisConfig.java
+++ src/main/java/com/hana/bankai/global/config/RedisConfig.java
@@ -2,6 +2,9 @@ package com.hana.bankai.global.config;
 
 
 import lombok.RequiredArgsConstructor;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -50,5 +53,15 @@ public class RedisConfig {
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashValueSerializer(new StringRedisSerializer());
         return redisTemplate;
+    }
+
+    // RedissonConfig
+    private static final String REDISSON_HOST_PREFIX = "redis://";
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort).setPassword(redisPwd);
+        return Redisson.create(config);
     }
 }
