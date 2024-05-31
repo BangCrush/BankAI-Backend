4d2cb73fbd9a518a6e558cf4256e052bee378e80
soyeonvv <sovvworld@gmail.com>
2024-05-31 16:56:01 +0900

    [refactor] SuccessCode 수정

--- src/main/java/com/hana/bankai/global/common/response/SuccessCode.java
+++ src/main/java/com/hana/bankai/global/common/response/SuccessCode.java
@@ -14,7 +14,7 @@ public enum SuccessCode {
     USER_REGISTER_CHECK_SUCCESS(OK, "회원가입 여부 확인 성공"),
     USER_REGISTER_CHECK_EMAIL_SUCCESS(OK, "이메일 중복 확인 성공"),
     USER_REGISTER_CHECK_ID_SUCCESS(OK, "아이디 중복 확인 성공"),
-    USER_REGISTER_SUCCESS(OK, "회원가입 성공"),
+    USER_REGISTER_SUCCESS(CREATED, "회원가입 성공"),
     USER_LOGIN_SUCCESS(OK, "로그인 성공"),
     USER_FIND_ID_SUCCESS(OK, "아이디 찾기 성공"),
     USER_FIND_PWD_SUCCESS(OK, "비밀번호 찾기 성공"),
@@ -22,7 +22,7 @@ public enum SuccessCode {
     SEND_EMAIL_CODE_SUCCESS(OK, "이메일 인증 코드 전송 성공"),
 
     // Account
-    ACCOUNT_CREATE_SUCCESS(OK,"계좌 개설 성공"),
+    ACCOUNT_CREATE_SUCCESS(CREATED,"계좌 개설 성공"),
     ACCOUNT_DELETE_SUCCESS(OK,"계좌 해지 성공"),
     ACCOUNT_BALANCE_CHECK_SUCCESS(OK, "계좌 잔액 조회 성공"),
     ACCOUNT_SEARCH_SUCCESS(OK, "계좌 조회 성공"),
