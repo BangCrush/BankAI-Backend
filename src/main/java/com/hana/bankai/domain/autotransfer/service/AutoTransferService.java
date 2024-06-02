e36ff71c56c2ddf0bd68aa4ebd65aff96bab95ea
LimEunSang <dmstkd2905@naver.com>
2024-06-02 20:27:22 +0900

    [feat] scheduler를 통한 자동이체 기능
    - 아직 테스트 못 해봤어요. 자정에 해볼게요.

---
+++ src/main/java/com/hana/bankai/domain/autotransfer/service/AutoTransferService.java
@@ -0,0 +1,54 @@
+package com.hana.bankai.domain.autotransfer.service;
+
+import com.hana.bankai.domain.account.dto.AccountRequestDto;
+import com.hana.bankai.domain.account.entity.Account;
+import com.hana.bankai.domain.account.service.AccountService;
+import com.hana.bankai.domain.autotransfer.entity.AutoTransfer;
+import com.hana.bankai.domain.autotransfer.repository.AutoTransferRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+public class AutoTransferService {
+
+    private final AutoTransferRepository autoTransferRepository;
+    private final AccountService accountService;
+
+    public void autoTransfer() {
+        // 오늘 날짜(며칠인지) 조회
+        int todayDay = LocalDate.now().getDayOfMonth();
+
+        // 자동이체 DB Table 에서 모든 데이터를 조회
+        List<AutoTransfer> autoTransferList = autoTransferRepository.findAll();
+
+        for(AutoTransfer autoTransfer : autoTransferList) {
+            // 오늘이 자동이체 날이면 자동이체 실행
+            if(autoTransfer.getAutoTransferId().getAtDate() != todayDay) continue;
+
+            // 출금 계좌 user ID 조회
+            Account outAccount = autoTransfer.getAccount();
+            String userId = outAccount.getUser().getUserId();
+
+            // 이체(Transfer) DTO 생성
+            AccountRequestDto.Transfer transfer = AccountRequestDto.Transfer.builder()
+                    .inAccCode(autoTransfer.getAutoTransferId().getInAccCode())
+                    .inBankCode(autoTransfer.getInBankCode())
+                    .outAccCode(autoTransfer.getAutoTransferId().getOutAccCode())
+                    .amount(autoTransfer.getAtAmount())
+                    .build();
+
+            // 이체 Service 호출
+            accountService.transfer(transfer, userId);
+        }
+    }
+
+}
+
--- src/main/java/com/hana/bankai/global/scheduler/BankaiScheduler.java
+++ src/main/java/com/hana/bankai/global/scheduler/BankaiScheduler.java
@@ -1,5 +1,6 @@
 package com.hana.bankai.global.scheduler;
 
+import com.hana.bankai.domain.autotransfer.service.AutoTransferService;
 import com.hana.bankai.domain.user.service.UserTrsfLimitService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,9 +11,13 @@ import org.springframework.stereotype.Component;
 public class BankaiScheduler {
 
     private final UserTrsfLimitService trsfLimitService;
+    private final AutoTransferService autoTransferService;
 
     @Scheduled(cron = "0 0 0 * * ?") // 매일 자정에 실행
     public void resetDailyUserTrsfLimit() {
         trsfLimitService.resetUserDailyTrsfLimit();
     }
+
+    @Scheduled(cron = "0 0 0 * * ?") // 매일 자정에 실행
+    public void autoTransfer() { autoTransferService.autoTransfer(); }
 }
