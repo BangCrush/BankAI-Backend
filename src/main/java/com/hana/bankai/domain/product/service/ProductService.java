649b8c92e815c01e841c0f6598412d42d0015e7c
LimEunSang <dmstkd2905@naver.com>
2024-06-04 23:14:50 +0900

    [chore] product domain 코드 정렬
    - controller, service 파일 intellij 코드 자동 정렬 적용
    - 너무 코드가 붙어있어서 가독성을 위해 줄바꿈도 좀 적용했습니다.

--- src/main/java/com/hana/bankai/domain/product/service/ProductService.java
+++ src/main/java/com/hana/bankai/domain/product/service/ProductService.java
@@ -1,12 +1,10 @@
 package com.hana.bankai.domain.product.service;
 
-import com.hana.bankai.domain.account.dto.AccountRequestDto;
 import com.hana.bankai.domain.account.repository.AccountRepository;
 import com.hana.bankai.domain.product.dto.ProductResponseDto;
 import com.hana.bankai.domain.product.entity.ProdType;
 import com.hana.bankai.domain.product.entity.Product;
 import com.hana.bankai.domain.product.repsoitory.ProductRepository;
-import com.hana.bankai.domain.user.entity.User;
 import com.hana.bankai.global.common.response.ApiResponse;
 import com.hana.bankai.global.error.exception.CustomException;
 import lombok.RequiredArgsConstructor;
@@ -27,64 +25,79 @@ import static com.hana.bankai.global.error.ErrorCode.PRODUCT_NOT_SEARCH;
 public class ProductService {
     private final ProductRepository productRepository;
     private final AccountRepository accountRepository;
-    
+
     // 상품 디테일 페이지
     public ApiResponse<Product> getProductDetail(Long prodCode) {
         Product p = productRepository.findById(prodCode)
                 .orElseThrow(() -> new CustomException(PRODUCT_NOT_SEARCH));
         return ApiResponse.success(PRODUCT_SEARCH_SUCCESS, p);
-    };
+    }
 
     // 상품 별 전체 조회 코드
-    public ApiResponse<Map<ProdType,List<ProductResponseDto.GetProduct>>> getProduct(int prodType) {
+    public ApiResponse<Map<ProdType, List<ProductResponseDto.GetProduct>>> getProduct(int prodType) {
         ProdType prodTypeNum = ProdType.of(prodType);
-        List<Product> productList = productRepository.findByProdType(prodTypeNum) ;
-        if (productList.isEmpty()){
+
+        List<Product> productList = productRepository.findByProdType(prodTypeNum);
+
+        if (productList.isEmpty()) {
             throw new CustomException(PRODUCT_NOT_SEARCH);
         }
-        List<ProductResponseDto.GetProduct> productDtoList =  new ArrayList<>();
-        for(Product p : productList){
+
+        List<ProductResponseDto.GetProduct> productDtoList = new ArrayList<>();
+
+        for (Product p : productList) {
             ProductResponseDto.GetProduct prodDto = ProductResponseDto.GetProduct.from(p);
             productDtoList.add(prodDto);
         }
+
         Map<ProdType, List<ProductResponseDto.GetProduct>> prodTypeListMap = new HashMap<>();
+
         prodTypeListMap.put(prodTypeNum, productDtoList);
+
         return ApiResponse.success(PRODUCT_SEARCH_SUCCESS, prodTypeListMap); // Pass the product object
-    };
+    }
 
     // 전체 조회 코드(타입별로 나눠서 전체를 보내줌)
     public ApiResponse<Map<ProdType, List<ProductResponseDto.GetProduct>>> productSearchAll() {
         List<Product> allProduct = productRepository.findAll();
+
         if (allProduct.isEmpty()) {
             throw new CustomException(PRODUCT_NOT_SEARCH);
         }
+
         Map<ProdType, List<ProductResponseDto.GetProduct>> responseMap = divisionProdByType(allProduct);
 
         return ApiResponse.success(PRODUCT_SEARCH_SUCCESS, responseMap);
     }
 
-    public ApiResponse<List<ProductResponseDto.GetTopThree>> prodTopThree(){
+    public ApiResponse<List<ProductResponseDto.GetTopThree>> prodTopThree() {
         List<Product> getProdTopThree = productRepository.findTopProducts();
-        if (getProdTopThree.isEmpty()){
+
+        if (getProdTopThree.isEmpty()) {
             throw new CustomException(PRODUCT_NOT_SEARCH);
         }
-        List<ProductResponseDto.GetTopThree> productDtoList =  new ArrayList<>();
-        for(Product p : getProdTopThree){
+
+        List<ProductResponseDto.GetTopThree> productDtoList = new ArrayList<>();
+
+        for (Product p : getProdTopThree) {
             ProductResponseDto.GetTopThree prodDto = ProductResponseDto.GetTopThree.from(p);
             productDtoList.add(prodDto);
         }
-        return ApiResponse.success(PRODUCT_SEARCH_SUCCESS,productDtoList);
+
+        return ApiResponse.success(PRODUCT_SEARCH_SUCCESS, productDtoList);
     }
 
-    public ApiResponse<Map<ProdType,List<ProductResponseDto.GetProduct>>> getProdSearch(String keyword) {
+    public ApiResponse<Map<ProdType, List<ProductResponseDto.GetProduct>>> getProdSearch(String keyword) {
         List<Product> getProdSearch = productRepository.findByProdNameContaining(keyword)
-                .orElseThrow(() -> new CustomException(PRODUCT_NOT_SEARCH)
-        );
-        if (getProdSearch.isEmpty()){
+                .orElseThrow(() -> new CustomException(PRODUCT_NOT_SEARCH));
+
+        if (getProdSearch.isEmpty()) {
             throw new CustomException(PRODUCT_NOT_SEARCH);
         }
+
         Map<ProdType, List<ProductResponseDto.GetProduct>> responseMap = divisionProdByType(getProdSearch);
-        return ApiResponse.success(PRODUCT_SEARCH_SUCCESS,responseMap);
+
+        return ApiResponse.success(PRODUCT_SEARCH_SUCCESS, responseMap);
     }
 
     // 타입별 분류
